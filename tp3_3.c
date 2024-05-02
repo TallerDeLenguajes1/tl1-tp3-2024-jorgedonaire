7900a4b1c6bd027865a698d169e4b5db68059a6e
Jorge Gregorio Donaire Pereyra <jorgedonaire93@gmail.com>
2024-05-02 10:31:19 -0300

    Terminado punto 3

--- tp3_3.c
+++ tp3_3.c
@@ -3,20 +3,32 @@
 #include <stdlib.h>
 
 int main(){
+    char *V[5];
     char **Buffer;
-    Buffer = (char**) malloc(5 * sizeof(char *));
+    // Buffer = (char**) malloc(5 * sizeof(char *));
 
     char* Buff;
     char* Nombre;
 
-    Buff = (char*) malloc(100 * sizeof(char));
-    printf("Ingrese el nombre: ");
-    gets(Buff);
+    // Nombre = (char*) malloc(100 * sizeof(char));
+    //Ingresa los nombres por pantalla
+    for (int i = 0; i < 5; i++)
+    {
+        Buff = (char*) malloc(100 * sizeof(char));
+        printf("Ingrese el nombre: ");
+        gets(Buff);
+        V[i] = (char *) malloc((strlen(Buff) + 1) * sizeof(char));
+        strcpy(V[i],Buff);
+        free(Buff);
+    }
 
-    strcpy(Nombre,Buff);
-    puts(Nombre);
-
-    free(Buff);
-    free(Nombre);
+    //Muestra los nombres por pantalla
+    for (int i = 0; i < 5; i++)
+    {
+        puts(V[i]);
+        printf("\n");
+        free(V[i]);
+    }
+    
     return 0;
 }
